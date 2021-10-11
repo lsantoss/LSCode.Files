26c7c54e932be4dfd9dea2007ea0f41013ae71d4
Lucas Santos <lucas.faria@viannasempre.com.br>
2021-10-11 16:06:15 -0300

    Melhoria na compactação de imagens

--- LSCode.Arquivos/Imagens/CompactadorImagens.cs
+++ LSCode.Arquivos/Imagens/CompactadorImagens.cs
@@ -7,6 +7,7 @@ namespace LSCode.Arquivos.Imagens
     public static class CompactadorImagens
     {
         /// <summary>Compacta imagem do caminho parametrizado.</summary>
+        /// <remarks>Observação: A imagem original será substituída pela imagem compactada. Pode existir perda de qualidade.</remarks>
         /// <param name="pathImagem">Caminho da imagem que será compactada.</param>
         /// <exception cref="Exception">Erro ao compactar a imagem.</exception>
         public static void Compactar(string pathImagem)
@@ -14,9 +15,9 @@ namespace LSCode.Arquivos.Imagens
             using (var imageMagick = new MagickImage(pathImagem))
             {
                 imageMagick.Transparent(MagickColor.FromRgb(0, 0, 0));
-                imageMagick.FilterType = FilterType.Quadratic;
+                imageMagick.FilterType = FilterType.Spline;
                 imageMagick.Resize(2520, 3500);
-                imageMagick.ColorType = ColorType.TrueColor;
+                imageMagick.ColorType = ColorType.Palette;
                 imageMagick.Format = MagickFormat.Png8;
                 imageMagick.Write(pathImagem);
             }
